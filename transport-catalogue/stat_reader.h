ed5f699410198bbace6b2c2b1b31de68c732515e
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2024-09-22 17:09:56 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,9 @@
-// место для вашего кода
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+#include "transport_catalogue.h"
+#include"input_reader.h"
+
+//обработка запросов к базе и вывод данных
+void ParseAndPrintStat(const TransportCatalogue& tansport_catalogue, std::string_view request, std::ostream& output);
