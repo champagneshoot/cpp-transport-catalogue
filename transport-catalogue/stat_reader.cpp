c9198e042153e8e823ff609e5d01155f5b9f414d
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2024-09-22 17:09:37 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,1 +1,49 @@
-// место для вашего кода
+#include "stat_reader.h"
+void ParseAndPrintStat(const TransportCatalogue& transport_catalogue, std::string_view request, std::ostream& output) 
+{
+    if (request.substr(0, 4) == "Bus ") {
+        std::string bus_name = std::string(request.substr(4));
+        if (auto bus_info = transport_catalogue.GetBusInfo(bus_name)) 
+        {
+            int total_stops = std::get<0>(*bus_info);
+            int unique_stops = std::get<1>(*bus_info);
+            double route_length = std::get<2>(*bus_info);
+            output << "Bus " << bus_name << ": " << total_stops << " stops on route, "
+                << unique_stops << " unique stops, " << route_length << " route length\n";
+        }
+        else 
+        {
+            output << "Bus " << bus_name << ": not found\n";
+        }
+    }
+    else if (request.substr(0, 5) == "Stop ") {
+        std::string stop_name = std::string(request.substr(5));
+        Stop* stop = transport_catalogue.FindStop(stop_name);
+        if (stop) 
+        {
+            const auto* buses = transport_catalogue.GetBusesByStop(stop_name);
+            if (buses && !buses->empty()) 
+            {
+                output << "Stop " << stop_name << ": buses";
+                for (const auto& bus : *buses) 
+                {
+                    output << " " << bus;  
+                }
+                output << "\n";
+            }
+            else 
+            {
+                output << "Stop " << stop_name << ": no buses\n";
+            }
+        }
+        else 
+        {
+            output << "Stop " << stop_name << ": not found\n";
+        }
+    }
+    else 
+    {
+        // Если запрос не распознан
+        output << "Unknown request type\n";
+    }
+}
