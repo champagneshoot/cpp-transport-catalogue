ec2c1379d704688de55bdfe7b61512ff85077345
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2024-09-29 06:02:18 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -22,6 +22,11 @@ struct Bus
 	std::string bus_name;
 	std::vector<Stop*> bus_stops;
 };
+class Hasher
+{
+public:
+	size_t operator()(const std::pair<Stop*, Stop*>& stop_pair) const;
+};
 class TransportCatalogue 
 {
 public:
@@ -29,12 +34,16 @@ public:
 	Stop* FindStop(std::string_view) const;
 	void AddBus(const std::string&, std::vector<std::string_view>);
 	Bus* FindBus(std::string_view) const;
-	std::optional<std::tuple<int, int, double>> GetBusInfo(const std::string_view) const;
+	std::optional<std::tuple<int, int, int ,double>> GetBusInfo(const std::string_view) const;
 	const std::set<std::string>* GetBusesByStop(std::string_view) const;
+
+	void AddDistance(Stop&, Stop&, int);
 private:
 	std::deque<Stop> stops;
 	std::unordered_map<std::string_view, Stop*> stopname_to_stop;
 	std::deque <Bus> buses;
 	std::unordered_map<std::string_view, Bus*> busname_to_bus;
 	std::unordered_map<Stop*, std::set<std::string>> stop_to_buses;
+
+	std::unordered_map<std::pair<Stop*, Stop*>, int, Hasher> distances_;
 };
