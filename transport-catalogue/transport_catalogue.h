2250df54a485b8e7230ec42facf8efe687247f15
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2024-10-28 15:50:37 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "geo.h"
-#include "input_reader.h"
 
+#include <algorithm>
 #include <deque>
 #include <unordered_map>
 #include <string>
@@ -10,17 +10,19 @@
 #include <set>
 #include <unordered_set>
 #include <iostream>
+#include <vector>
 
 
 struct Stop
 {
 	std::string stop_name;
-	Coordinates stop_coordinates;
+	geo::Coordinates stop_coordinates;
 };
 struct Bus
 {
 	std::string bus_name;
 	std::vector<Stop*> bus_stops;
+	bool is_roundtrip;
 };
 class Hasher
 {
@@ -34,19 +36,50 @@ struct BusInfo
 	int full_route_length;
 	double curvature;
 };
-class TransportCatalogue 
+class TransportCatalogue
 {
 public:
-	void AddStop(const std::string&, Coordinates);
+	void AddStop(const std::string&, geo::Coordinates);
 	Stop* FindStop(std::string_view) const;
-	void AddBus(const std::string&, std::vector<std::string_view>);
+	void AddBus(const std::string&, std::vector<std::string_view>, bool);
 	Bus* FindBus(std::string_view) const;
 	std::optional<BusInfo> GetBusInfo(const std::string_view) const;
-	const std::set<std::string>* GetBusesByStop(std::string_view) const; // функция нужна для того, чтобы по названию остановки получить список автобусы, которые на ней останавливаются
-	                                                                     // задание звучало так: Добавьте в класс TransportCatalogue метод для получения списка автобусов по остановке.
+	const std::set<std::string>* GetBusesByStop(std::string_view) const;
+	/*если использовать const std::set<std::string>_view& GetBusesByStop(std::string_view) const;
+	то получается такой вывод:
+	Stop Biryulyovo Zapadnoye
+	Stop Biryulyovo Zapadnoye: buses ||| |||
+	это значит, что она ссылается на данные, которые больше не существуют или были изменены.
+	Я совсем не могу понять где это происходит
+	*/
+
 	void AddDistance(const Stop*, const Stop*, int);
 	int CalculateFullRouteLength(const Bus* bus) const;
-	int RouthLenghtBetweenTwoStops(const Stop*, const Stop*) const;
+	int RouteLenghtBetweenTwoStops(const Stop*, const Stop*) const;
+
+
+	std::vector<const Bus*> GetAllBuses() const
+	{
+		std::vector<const Bus*> all_buses;
+		all_buses.reserve(buses.size());
+		for (const auto& bus : buses)
+		{
+			all_buses.push_back(&bus);
+		}
+		std::sort(all_buses.begin(), all_buses.end(), [](const Bus* a, const Bus* b) { return a->bus_name < b->bus_name; });
+		return all_buses;
+	}
+
+	std::vector<const Stop*> GetAllStops() const
+	{
+		std::vector<const Stop*> all_stops;
+		all_stops.reserve(stops.size());
+		for (const auto& stop : stops)
+		{
+			all_stops.push_back(&stop);
+		}
+		return all_stops;
+	}
 
 private:
 	std::deque<Stop> stops;
@@ -54,6 +87,5 @@ private:
 	std::deque <Bus> buses;
 	std::unordered_map<std::string_view, Bus*> busname_to_bus;
 	std::unordered_map<Stop*, std::set<std::string>> stop_to_buses;
-
 	std::unordered_map<std::pair<const Stop*, const Stop*>, int, Hasher> distances_;
 };
