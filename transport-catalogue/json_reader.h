1c754d05f145bfcb4c86ae4a138249907cd40c4d
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2024-10-28 15:48:42 +0300

    Update and rename stat_reader.cpp to json_reader.h

---
+++ transport-catalogue/json_reader.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <sstream>
+
+#include "transport_catalogue.h"
+#include "json.h"
+#include "map_renderer.h"
+
+class InformationProcessing
+{
+public:
+
+    InformationProcessing(TransportCatalogue& catalogue, std::istream& input_stream_, std::ostream& out_);
+
+    void Process()
+    {
+        json::Document doc = json::Load(input_stream);
+        json::Node root = doc.GetRoot();
+
+        const auto& base_requests = root.AsMap().at("base_requests").AsArray();
+        ProcessBaseRequests(base_requests);
+
+        const auto& render_settings = root.AsMap().at("render_settings").AsMap();
+        ProcessRendererSet(render_settings);
+        svg::Document svg_doc;
+        MapRenderer mr(set);
+        mr.RenderStopsAndBuses(catalogue_, svg_doc);
+        svg_doc.Render(os);
+
+        const auto& stat_requests = root.AsMap().at("stat_requests").AsArray();
+        if (!stat_requests.empty())
+            ProcessStatRequests(stat_requests);
+    }
+
+    void ProcessBaseRequests(const json::Array& base_requests);
+    void ProcessStatRequests(const json::Array& stat_requests);
+    void ProcessRendererSet(const json::Dict& renderer_settings);
+
+private:
+    TransportCatalogue catalogue_;
+    Settings set;
+    std::istream& input_stream;
+    std::ostream& out;
+    std::ostringstream os;
+
+    svg::Color ProcessColor(const json::Node& color_node);
+
+    void ProcessStop(const json::Dict& stop_data);
+    void ProcessStopWithDistance(const json::Dict& stop_data);
+    void ProcessBus(const json::Dict& bus_data);
+
+    void ProcessStopRequest(const json::Dict& stop_request, json::Array& response_array);
+    void ProcessBusRequest(const json::Dict& bus_request, json::Array& response_array);
+    void ProcessMapRequest(const json::Dict& map_request, json::Array& response_array);
+};
