8a6d3d9ffa1a7dca1ec1b214c89dcf1748c5c319
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2024-10-28 15:47:42 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,33 +1,15 @@
 #include <iostream>
-#include <string>
-
-#include "input_reader.h"
-#include "stat_reader.h"
+#include <fstream>
+#include "json_reader.h"
 #include "transport_catalogue.h"
 
-using namespace std;
-
-int main() {
+int main() 
+{
     TransportCatalogue catalogue;
-
-    int base_request_count;
-    cin >> base_request_count >> ws;
-
-    {
-        InputReader reader;
-        for (int i = 0; i < base_request_count; ++i) {
-            string line;
-            getline(cin, line);
-            reader.ParseLine(line);
-        }
-        reader.ApplyCommands(catalogue);
-    }
-
-    int stat_request_count;
-    cin >> stat_request_count >> ws;
-    for (int i = 0; i < stat_request_count; ++i) {
-        string line;
-        getline(cin, line);
-        ParseAndPrintStat(catalogue, line, cout);
-    }
+    std::ifstream input_file("Текст.txt");
+    //InformationProcessing processor(catalogue, std::cin, std::cout);
+    InformationProcessing processor(catalogue, input_file, std::cout);
+    processor.Process();
+    return 0;
 }
+
