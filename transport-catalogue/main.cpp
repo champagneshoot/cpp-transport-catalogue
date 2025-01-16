00a37c50c0912ae169e72315566c766645787d0b
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2025-01-16 13:34:31 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -3,10 +3,12 @@
 #include "json_reader.h"
 #include "transport_catalogue.h"
 
-int main() 
+int main()
 {
     TransportCatalogue catalogue;
     std::ifstream input_file("Текст.txt");
+    //std::ifstream input_file("Пример1.txt");
+
     //InformationProcessing processor(catalogue, std::cin, std::cout);
     InformationProcessing processor(catalogue, input_file, std::cout);
     processor.Process();
