19b694d695d064a8800d091940790c91facd9c16
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2025-01-16 13:55:25 +0300

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -1,48 +1,52 @@
 #pragma once
 
-#include "router.h"
-#include "transport_catalogue.h"
 #include <map>
 #include <memory>
 #include <string_view>
 #include <optional>
 #include <vector>
 
+#include "router.h"
+#include "transport_catalogue.h"
 
+struct RouttingSettings
+{
+    int bus_wait_time_;
+    double bus_velocity_;
+};
 
-    struct RouteItem 
+struct RouteItem 
+{
+    enum class ItemType 
     {
-        enum class ItemType 
-        {
-            Wait,
-            Bus
-        };
-        ItemType type;
-        std::string name; 
-        double time;
-        size_t span_count; 
+        Wait,
+        Bus
     };
+    ItemType type;
+    std::string name; 
+    double time;
+    size_t span_count; 
+};
 
-    struct RouteResult {
-        double total_time;
-        std::vector<RouteItem> items;
-    };
+struct RouteResult 
+{
+    double total_time;
+    std::vector<RouteItem> items;
+};
 
-    class TransportRouter {
-    public:
-        TransportRouter(int bus_wait_time = 0, double bus_velocity = 0.0);
+class TransportRouter {
+public:
+    TransportRouter(RouttingSettings routting_settings);
+    
+    void BuildGraph(const TransportCatalogue& catalogue);   
+    std::optional<RouteResult> FindRoute(std::string_view stop_from, std::string_view stop_to) const;
 
-        void BuildGraph(const TransportCatalogue& catalogue);
-        std::optional<RouteResult> FindRoute(std::string_view stop_from, std::string_view stop_to) const;
+private:
+    void InitializeStops(const TransportCatalogue& catalogue);
+    void AddBusEdges(const TransportCatalogue& catalogue);
 
-    private:
-        void InitializeStops(const TransportCatalogue& catalogue);
-        void AddBusEdges(const TransportCatalogue& catalogue);
-
-        int bus_wait_time_;
-        double bus_velocity_;
-
-        graph::DirectedWeightedGraph<double> graph_;
-        std::map<std::string_view, graph::VertexId> stop_ids_;
-        std::unique_ptr<graph::Router<double>> router_;
-    };
+    graph::DirectedWeightedGraph<double> graph_;
+    std::map<std::string_view, graph::VertexId> stop_ids_;
+    std::unique_ptr<graph::Router<double>> router_;
+    RouttingSettings routting_settings_;
+};
