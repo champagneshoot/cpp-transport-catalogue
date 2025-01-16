651ff6ebe31add978f169ce3063dfe5d2ddc4f9b
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2025-01-16 14:20:33 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -6,10 +6,10 @@
 int main()
 {
     TransportCatalogue catalogue;
-    std::ifstream input_file("Текст.txt");
+    //std::ifstream input_file("Текст.txt");
     //std::ifstream input_file("Пример1.txt");
 
-    //InformationProcessing processor(catalogue, std::cin, std::cout);
+    InformationProcessing processor(catalogue, std::cin, std::cout);
     InformationProcessing processor(catalogue, input_file, std::cout);
     processor.Process();
     processor.ProcessRequest();
