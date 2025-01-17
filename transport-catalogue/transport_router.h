e7131c95fd9bcd9676c42454fa541da181648fd3
champagneshoot <162992559+champagneshoot@users.noreply.github.com>
2025-01-17 07:21:56 +0300

    Update transport_router.h

--- transport-catalogue/transport_router.h
+++ transport-catalogue/transport_router.h
@@ -28,9 +28,7 @@ struct RouteResult {
 
 class TransportRouter {
 public:
-    TransportRouter(int bus_wait_time = 0, double bus_velocity = 0.0);
-
-    void BuildGraph(const TransportCatalogue& catalogue);
+    TransportRouter(const TransportCatalogue& catalogue, int bus_wait_time = 0, double bus_velocity = 0.0);
     std::optional<RouteResult> FindRoute(std::string_view stop_from, std::string_view stop_to) const;
 
 private:
@@ -44,6 +42,7 @@ private:
     std::map<std::string_view, graph::VertexId> stop_ids_;
     std::unique_ptr<graph::Router<double>> router_;
 };
+
 
 
 
